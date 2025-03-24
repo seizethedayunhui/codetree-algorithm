ff14d613e3d640e39c9e522d69a6768c88d11fa8
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-03-24 13:42:10 +0900

    Title: 최댓값 구하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

--- 250324/최댓값 구하기/maximum-value.py
+++ 250324/최댓값 구하기/maximum-value.py
@@ -1,13 +1,15 @@
 a, b, c = map(int, input().split())
 
-if a >= b :
-    if a >= c :
+# a와 b를 비교한뒤, a가 b보다 크다면 a와 c를 비교하여 최댓값을 구합니다.
+if a >= b:
+    if a >= c:
         print(a)
+    else:
+        print(c)
 
-if b >= a :
-    if b >= c :
+# a와 b를 비교한 결과가 나와있으므로, b와 c만 비교하여 최댓값을 구합니다.
+else:
+    if b >= c:
         print(b)
-
-if c >= a :
-    if c >= b :
+    else:
         print(c)
