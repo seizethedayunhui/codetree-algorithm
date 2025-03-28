932c6b576777f7b8a74e44599daa225f5ad548db
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-03-28 23:54:33 +0900

    Title: 별표 출력하기 10, Time: 41ms, Memory: 16MB, Status: Passed - Codetree

--- 250328/별표 출력하기 10/print-star-10.py
+++ 250328/별표 출력하기 10/print-star-10.py
@@ -2,26 +2,15 @@ N = int(input())
 
 odd = 1
 even = N
-for i in range(1, N+1) :
-    if i % 2 :
-        for _ in range(odd):
-            print("*", end=" ")
-        odd += 1
-    else :
-        for _ in range(even):
-            print("*", end=" ")
-        even -= 1
-    print()
+for i in range(1, 2*N+1) :
+        if i % 2 :
+            for _ in range(odd):
+                print("*", end=" ")
+            odd += 1
+        else :
+            for _ in range(even):
+                print("*", end=" ")
+            even -= 1
+        print()
 
-odd = 4
-even = 3
-for i in range(1, N+1) :
-    if i % 2 :
-        for _ in range(odd-1):
-            print("*", end=" ")
-        odd -= 1
-    else :
-        for _ in range(even+1):
-            print("*", end=" ")
-        even += 1
-    print()
+
