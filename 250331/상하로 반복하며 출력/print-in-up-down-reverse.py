125380c01be91582fc6c209632e42d07efd8da14
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-03-31 22:27:09 +0900

    Title: 상하로 반복하며 출력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250331/상하로 반복하며 출력/print-in-up-down-reverse.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n) :
+
+    for j in range(n) :
+        
+        if j % 2 :
+            print( n - i , end="")
+        else:
+            print( i + 1 , end="")
+    print()
