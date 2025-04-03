1d6d6868d1d27c1ad4507d4896585e49048a511f
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-03 16:19:05 +0900

    Title: 연속부분수열일까, Time: 75ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250403/연속부분수열일까/contiguous-array-or-not.py
@@ -0,0 +1,21 @@
+n1, n2 = map(int, input().split())
+
+n1_arr = list(map(int, input().split()))
+n2_arr = list(map(int, input().split()))
+
+
+for i in range(n1) :
+    flag = 1
+    for j in range(n2) :
+        # 바깥배열의 값과 안의 배열이 다른 경우 flag 바꾸고 break
+        if n1_arr[i+j] != n2_arr[j] :
+            flag= 0
+            break
+    # flag가 1이면 이미 부분집합이라는걸 확인했으므로 더 이상 반복할 필요가 없음. 
+    if flag :
+        break
+if flag :
+    print("Yes")
+else :
+    print("No")
+            
