e666f809fda11db74dbbd495c8922dbe77992d85
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-03 16:23:10 +0900

    Title: 연속부분수열일까, Time: 40ms, Memory: 16MB, Status: Passed - Codetree

--- 250403/연속부분수열일까/contiguous-array-or-not.py
+++ 250403/연속부분수열일까/contiguous-array-or-not.py
@@ -7,12 +7,9 @@ n2_arr = list(map(int, input().split()))
 for i in range(n1) :
     flag = 1
     for j in range(n2) :
-        if not (i+j < n1) :
+        # 인덱스 범위 벗어나는 경우 + 바깥배열의 값과 안의 배열이 다른 경우 flag 바꾸고 break
+        if (i+j >= n1) or n1_arr[i+j] != n2_arr[j]:
             flag = 0
-            break
-        # 바깥배열의 값과 안의 배열이 다른 경우 flag 바꾸고 break
-        if n1_arr[i+j] != n2_arr[j] :
-            flag= 0
             break
     # flag가 1이면 이미 부분집합이라는걸 확인했으므로 더 이상 반복할 필요가 없음. 
     if flag :
