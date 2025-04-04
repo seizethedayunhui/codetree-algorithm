f417df2536972cfaebdd49805805fc91a6de53c6
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-04 15:45:34 +0900

    Title: N개의 정수 중 최대 2개, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250404/N개의 정수 중 최대 2개/two-max-of-n-num.py
+++ 250404/N개의 정수 중 최대 2개/two-max-of-n-num.py
@@ -1,12 +1,17 @@
 N = int(input())
 num_list = list(map(int, input().split()))
 
-max_1st = num_list[0]
-max_2nd = num_list[0]
+max_1st = float('-inf')
+max_2nd = float('-inf')
 
 for num in num_list :
     if num > max_1st :
+
+        # max_1st 이던 값을 max_2nd로 정정해주어야함. 
+        # 해주지 않으면 원소가 2개인 경우 에러남. 
+        max_2nd = max_1st
         max_1st = num
+
     elif num > max_2nd :
         max_2nd = num   
     
