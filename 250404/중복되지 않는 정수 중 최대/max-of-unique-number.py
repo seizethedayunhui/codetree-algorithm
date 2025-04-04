46d9a15bdfc80b54437fca0886418cdd0a6a97b3
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-04 16:45:42 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 52ms, Memory: 17MB, Status: Passed - Codetree

--- 250404/중복되지 않는 정수 중 최대/max-of-unique-number.py
+++ 250404/중복되지 않는 정수 중 최대/max-of-unique-number.py
@@ -1,35 +1,20 @@
 N = int(input())
 num_list = list(map(int, input().split()))
 
-dup_list = list()
-prev_list = list()
 
 max_val = -1
 prev_max_val = -1
 
+# 카운팅 하는 방식으로 구현함
 for num in num_list :
-    
-    if num == max_val :
-        # 중복되는 값이 나온 경우 / 중복 배열에 넣음. 
-        dup_list.append(num)
-        max_val = prev_max_val
-        if max_val in dup_list  :
-            max_val = -1
-        continue
-    elif num in prev_list :
-        dup_list.append(num)
-        continue
-
-    # 값이 max인 경우
-    # max로 설정 후 prev_max도 설정
     if num > max_val :
-        prev_max_val = max_val
-        max_val = num 
-    # 아닌데 prev_max 보다는 큰 경우 prev_max로 설정
-    elif num > prev_max_val :
-        prev_max_val = num
-    # 이전에 지나간 값을 넣음. 
-    prev_list.append(num)
-
+        cnt = 0
+        for elem in num_list:
+            if elem == num :
+                cnt += 1
+        if cnt == 1 :
+            max_val = num
+            
+    
     
 print(max_val)
