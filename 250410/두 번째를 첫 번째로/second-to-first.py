d00320eb750ab733d8f553e47f77a57c1d4fa975
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-10 22:49:26 +0900

    Title: 두 번째를 첫 번째로, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250410/두 번째를 첫 번째로/second-to-first.py
@@ -0,0 +1,11 @@
+string = input()
+
+char1 = string[0]
+char2 = string[1]
+
+string_list = list(string)
+
+for i in range(len(string_list)) :
+    if string_list[i] == char2 :
+        string_list[i] = char1
+print("".join(string_list))
