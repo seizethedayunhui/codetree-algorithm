931c553ac8d3d39ee1a06c619598173b49a542dc
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-10 22:58:02 +0900

    Title: 문자 제거하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250410/문자 제거하기/del-char.py
+++ 250410/문자 제거하기/del-char.py
@@ -3,7 +3,7 @@ string_list = list(string)
 
 for _ in range(len(string_list)-1) :
     index = int(input())
-    if index > len(string_list) :
+    if index >= len(string_list) :
         removed = string_list.pop(-1)
     else :
         removed = string_list.pop(index)
