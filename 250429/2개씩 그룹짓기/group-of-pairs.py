816e2db0e87a0d8e3f90e8e38981a44a168db27b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-29 17:55:17 +0900

    Title: 2개씩 그룹짓기, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250429/2개씩 그룹짓기/group-of-pairs.py
+++ 250429/2개씩 그룹짓기/group-of-pairs.py
@@ -2,22 +2,15 @@ N = int(input())
 
 N_list = list(map(int, input().split()))
 
-sum_max_min = float('inf')
+N_list.sort()
 
-for i in range(2) :
+sum_max = float('-inf')
 
-    sum_max = float('-inf')
+for i in range(N) :
 
-    for j in range(i, N, 2) :
-        if j+1 >= N :
-            current_max = N_list[j] + N_list[0]
-        else :
-            current_max = N_list[j] + N_list[j+1]
+    current_sum = N_list[i] + N_list[ 2*N-(i+1) ]
 
-        if current_max > sum_max :
-            sum_max = current_max
+    if current_sum > sum_max :
+        sum_max = current_sum
 
-    if sum_max < sum_max_min :
-        sum_max_min = sum_max
-
-print(sum_max_min)
+print(sum_max)
