f4745c48e82338b259af2e08dd7a3b6c58e40a80
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-04-30 12:30:55 +0900

    Title: Next Level, Time: 44ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250430/Next Level/next-level.py
@@ -0,0 +1,10 @@
+class Info:
+    def __init__(self, id_info = "codetree", level_info = 10):
+        self.id_info = id_info
+        self.level_info = level_info
+
+user1 = Info()
+user2 = Info("hello", 28)
+
+print(f"user {user1.id_info} lv {user1.level_info}")
+print(f"user {user2.id_info} lv {user2.level_info}")
