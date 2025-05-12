8238fc4f562f2edfcd91cd4c1f0a74d4c8a9e8d3
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-12 10:27:27 +0900

    Title: 연속되는 수 3, Time: 53ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/연속되는 수 3/continuous-number3.py
+++ 250512/연속되는 수 3/continuous-number3.py
@@ -16,12 +16,13 @@ for i in range(N) :
         cnt = 1
 
     elif ( N_list[i] < 0 and N_list[i] < flag ) or ( N_list[i] > 0 and N_list[i] > flag ) :
-        if cnt > max_len :
-            max_len = cnt
         cnt = 1
 
     else :
         cnt += 1
+
+    if cnt > max_len :
+        max_len = cnt
     
 
 
