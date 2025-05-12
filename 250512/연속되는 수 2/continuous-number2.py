a06a3a3329bdbd9bf7c8a4285d0ffe9a4b4352a2
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-12 10:10:45 +0900

    Title: 연속되는 수 2, Time: 63ms, Memory: 16MB, Status: Passed - Codetree

--- 250512/연속되는 수 2/continuous-number2.py
+++ 250512/연속되는 수 2/continuous-number2.py
@@ -13,9 +13,12 @@ for _ in range(N) :
 cnt = 1 # 초기화 자체를 1로 해줌
 for i in range(N) :
     if i == 0 or (N_list[i] != N_list[i-1]) :
-        if cnt > max_range :
-            max_range = cnt
+        cnt = 1
     else :
         cnt += 1
+    # 리스트의 last 원소까지 세어야 하는 경우가 있을 수도 있기 때문에
+    # 중첩 if 문이 아니라, 각각의 if문으로 처리
+    if cnt > max_range :
+        max_range = cnt
 
 print(max_range)
