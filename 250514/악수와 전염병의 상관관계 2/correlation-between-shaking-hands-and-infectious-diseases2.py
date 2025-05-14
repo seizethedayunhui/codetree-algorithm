355b7e56aa4e1adb94c5509f21414e80eff10e5c
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-14 23:54:04 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 49ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250514/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250514/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -9,9 +9,8 @@ commands = list()
 
 for _ in range(T) :
     t, x, y = map(int, input().split())
-    cnt = 0
 
-    command = [t, x, y, cnt]
+    command = [t, x, y]
     commands.append(command)
 
 # 시간 순서대로 정렬
@@ -19,26 +18,26 @@ commands.sort(key = lambda x : x[0])
 
 # memo -> [감염여부, 악수횟수]
 memo = [ [0, 0] for _ in range(N) ]
-memo[K-1][0] = 1
+memo[P-1][0] = 1
 
 for command in commands :
 
     # 개발자 번호
     x = command[1] - 1
     y = command[2] - 1
+
     # 감염되었는지 확인 & 감염횟수 만족하는지 확인 -> 개발자 별로 각각 진행
     if memo[x][0] and memo[x][1] < K :
 
         # 감염된 사람끼리 만난 경우
-        memo[y][1] += 1
         memo[y][0] = 1
+        memo[y][1] += 1
         memo[x][1] += 1
 
     elif memo[y][0] and memo[y][1] < K :
         # 감염된 사람끼리 만난 경우
-
-        memo[x][1] += 1
         memo[x][0] = 1
+        memo[x][1] += 1
         memo[y][1] += 1     
 
 for elem in memo :
