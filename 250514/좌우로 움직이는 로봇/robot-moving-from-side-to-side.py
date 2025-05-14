0b993d0e7d33df7929be239be44e41f67c0517a0
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-14 23:12:35 +0900

    Title: 좌우로 움직이는 로봇, Time: 307ms, Memory: 75MB, Status: Passed - Codetree

--- 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250514/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -43,22 +43,47 @@ cnt = 0
 # 최대를 길이를 설정
 move_len = max(len(A_move), len(B_move))
 
+
+A_flag = False
+B_flag = False
 for i in range(move_len) :
 
     # 범위 조정 설정
-    if len(A_move) <= i+1 :
+    # 플래그로 구분
+
+    # flag : 어느 배열에서 범위가 끝났는지 확인
+    # #_idx : 이전의 위치를 알기 위한 idx
+    # A_i : 현재의 로봇 위치
+    
+    if len(A_move) < i+1 :
         A_i = len(A_move) - 1
+        A_flag = True
     else :
         A_i = i
     
-    if len(B_move) <= i+1 :
+    if len(B_move) < i+1 :
         B_i = len(B_move) - 1
+        B_flag = True
     else :
         B_i = i
 
     # 그냥 보통의 경우
-    if (i != 0) and (A_move[A_i-1] != B_move[B_i-1]) :
-        if A_move[A_i] == B_move[B_i] :
-            cnt += 1  
+    if (i != 0) :
+
+        A_idx = i-1
+        B_idx = i-1
+
+        # flag를 통해 어느 배열이 끝났는지 파악함
+        if (A_flag == True) :
+            # 끝난 위치로 인덱스 설정
+            A_idx = A_i
+        elif (B_flag == True) :
+            # 끝난 위치로 인덱스 설정
+            B_idx = B_i
+
+        # 이전과 다른 위치라면 cnt += 1
+        if (A_move[A_idx] != B_move[B_idx]) :
+            if A_move[A_i] == B_move[B_i] :
+                cnt += 1 
 
 print(cnt)
