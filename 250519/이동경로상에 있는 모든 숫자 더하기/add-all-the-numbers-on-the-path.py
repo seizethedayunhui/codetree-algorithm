585582c279113a7b5ea24a9b05c0cb4a12e30bb7
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-19 23:14:08 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 90ms, Memory: 21MB, Status: Runtime Error - Codetree

---
+++ 250519/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.py
@@ -0,0 +1,73 @@
+N, T = map(int, input().split())
+
+command = list(input())
+
+cnt = 1
+
+mat = list()
+for _ in range(N) :
+    row = list()
+    for _ in range(N):
+        row.append(cnt)
+        cnt += 1
+    mat.append(row)
+
+"""
+
+    L : 왼쪽으로 90도 방향 전환
+    R : 오른쪽으로 90도 방향 전환
+    F : 바라보는 방향 한칸 이동
+
+"""
+
+dx = [ 0, 1, 0, -1 ]
+dy = [ 1, 0, -1, 0 ]
+direc_num = 3
+
+# 범위 벗어나는지 확인하는 함수
+def in_range(x, y, N):
+    return ( 0 <= x < N ) and ( 0 <= y < N ) 
+
+x, y = N // 2, N // 2
+ans = 0
+
+
+for elem in command :
+
+
+    if elem == "L":
+        # 왼쪽으로 90도 방향 전환
+        direc_num = ( direc_num + 3 ) % 4
+
+
+    elif elem == "R" :
+        # 오른쪽으로 90도 방향 전환
+        direc_num = ( direc_num + 1 ) % 4
+
+    else :
+
+        nx = x + dx[direc_num]
+        ny = y + dy[direc_num]
+
+        # 이동 자체를 하면 안됨
+        if not in_range(nx, ny, N):
+            continue
+
+    ans += mat[x][y]
+
+    
+    
+    x += dx[direc_num]
+    y += dy[direc_num]
+
+
+print(ans)
+
+
+    
+
+
+    
+
+    
+
