4ae09ecc95c1e6bbc68bfc3c41ae3d7a385220f9
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-20 18:11:41 +0900

    Title: 이상한 진수 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250520/이상한 진수 2/awkward-digits-2.py
+++ 250520/이상한 진수 2/awkward-digits-2.py
@@ -7,13 +7,22 @@ for i in range(a_len) :
 
 max_N = float('-inf')
 
-for i in range(a_len) :
+for i in range(1, a_len) :
 
     a_sum = 0
 
+    if a[i] == 0 :
+        a[i] += 1
+    else :
+        a[i] -= 1
+
     for j in range(a_len) :
-        if (i != j) :
-            a_sum += (2 ** (a_len-(j+1)))
+            a_sum += (2 ** (a_len-(j+1))) * a[j]
+
+    if a[i] :
+        a[i] -= 1
+    else :
+        a[i] += 1
 
     max_N = max(max_N, a_sum)
 
