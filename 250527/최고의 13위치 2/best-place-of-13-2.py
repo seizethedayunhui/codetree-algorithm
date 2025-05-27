afa5514bfd676b1bd463c76f7740ca4257c6a19d
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-27 22:56:01 +0900

    Title: 최고의 13위치 2, Time: 57ms, Memory: 17MB, Status: Passed - Codetree

--- 250527/최고의 13위치 2/best-place-of-13-2.py
+++ 250527/최고의 13위치 2/best-place-of-13-2.py
@@ -13,20 +13,26 @@ def in_range(x, y, N) :
 max_cnt = float('-inf')
 for x1 in range(N) :
     for y1 in range(N-2) :
-        for x2 in range(x1+1, N) :
-
-
+        for x2 in range(N) :
             for y2 in range(N-2) :
-
                 # 여기서 0으로 초기화 해주어야함. 
                 cnt1 = 0
                 cnt2 = 0
                 cnt = 0
 
+                # 겹치는 경우 생각하기
+                if x1 == x2 and abs(y2-y1) < 3 :
+                    break
+
                 # 실질적으로 cnt하는 곳
                 for i in range(3) :
-                    cnt1 += mat[x1 + dx[i]][y1 + dy[i]]
-                    cnt2 += mat[x2 + dx[i]][y2 + dy[i]]
+
+                    nx1 = x1 + dx[i]
+                    ny1 = y1 + dy[i]
+                    nx2 = x2 + dx[i]
+                    ny2 = y2 + dy[i]
+                    cnt1 += mat[nx1][ny1]
+                    cnt2 += mat[nx2][ny2]
 
                 cnt = cnt1 + cnt2
 
