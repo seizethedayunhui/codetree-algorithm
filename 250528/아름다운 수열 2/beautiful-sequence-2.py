526fa1eb9dac9e74cd415717f0c9ef919d0686a8
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-28 21:53:51 +0900

    Title: 아름다운 수열 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250528/아름다운 수열 2/beautiful-sequence-2.py
+++ 250528/아름다운 수열 2/beautiful-sequence-2.py
@@ -4,12 +4,21 @@ N, M = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
+B.sort()
+
 cnt = 0
 
 for i in range( N - M + 1 ):
-    flag = True
-    for j in range(i, i + M) :
-        if A[j] not in B :
+
+    cmp_list = list()   
+    flag = True 
+    for j in range(i, i+M) :
+        cmp_list.append(A[j])
+
+    cmp_list.sort()
+
+    for k in range(M) :
+        if cmp_list[k] != B[k] :
             flag = False
             break
     if flag :
