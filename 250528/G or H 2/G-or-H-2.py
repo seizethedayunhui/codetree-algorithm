61fe56e78675aea9df992f87ba570bac352ade0b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-28 23:14:36 +0900

    Title: G or H 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250528/G or H 2/G-or-H-2.py
+++ 250528/G or H 2/G-or-H-2.py
@@ -34,25 +34,27 @@ for i in range(N) :
         if N_list[j][1] == "H" :
             H_cnt += 1
         
-        if N_list[j][1] != current_char :
+        # 계속 같은 문자만 이어지는 경우를 고려하여, ==을 조건으로 삼음(!= 대신에)
+        if N_list[j][1] == current_char :
             
-            # 같은 문자인지 확인하기 위한 기록
-            current_char = N_list[j][1]
-
             # 같은 문자끼리 있는 구간의 길이
-            current_char_dis = (j-1) - start_char_idx
-
-            # 인덱스 갱신
-            start_char_idx = j
+            current_char_dis = N_list[j][0] - N_list[start_char_idx][0]
 
             # 같은 문자끼리 있는 구간의 최대 크기만 저장
             same_char_dis = max(same_char_dis, current_char_dis)
+
+        else :
+            # 같은 문자인지 확인하기 위한 기록
+            current_char = N_list[j][1]
+            # 인덱스 갱신
+            start_char_idx = j
 
         if G_cnt == H_cnt :
 
             current_cnt_dis = N_list[j][0] - N_list[i][0]
             same_cnt_dis = max(same_cnt_dis, current_cnt_dis)
 
+    # print(same_char_dis, same_cnt_dis)
     if same_cnt_dis >= same_char_dis :
         max_dis = max(max_dis, same_cnt_dis)
     else :
