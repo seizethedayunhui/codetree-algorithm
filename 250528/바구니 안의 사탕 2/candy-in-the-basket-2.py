27a674d687e7fd63c145a13af90faab5142c6e1b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-28 22:33:05 +0900

    Title: 바구니 안의 사탕 2, Time: 50ms, Memory: 17MB, Status: Passed - Codetree

--- 250528/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 250528/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -4,13 +4,16 @@ N_cnt = [ 0 for _ in range(401) ]
 
 for _ in range(N) :
     candy, l = map(int, input().split())
-    N_cnt[l] = candy
+
+    # 같은 위치에 바구니가 있을 수도 있음!
+    N_cnt[l] += candy
+
 
 max_sum = float('-inf')
-for i in range(K, 401 - K - 1) :
+for i in range(K , 401 - K) :
 
     interal_sum = 0
-    for j in range(i-K, i + K + 1):
+    for j in range(i-K, i + K+1):
         interal_sum += N_cnt[j]
     max_sum = max(max_sum, interal_sum)
 
