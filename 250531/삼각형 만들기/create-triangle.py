e26170cca5a0d5d630fff6926e1d662961a3563b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-05-31 23:51:02 +0900

    Title: 삼각형 만들기, Time: 54ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 250531/삼각형 만들기/create-triangle.py
@@ -0,0 +1,26 @@
+N = int(input())
+points = list()
+
+for _ in range(N) :
+    x, y = map(int, input().split())
+    points.append((x, y))
+
+max_traingle = float('-inf')
+
+for i in range(N) :
+    for j in range(N) :
+        for k in range(N) :
+
+            if i == j or j == k or k == i :
+                break
+
+            # 한 변은 x 축에 평행 & 다른 한 변은 y 축게 평행
+            if points[i][0] == points[j][0] and points[j][1] == points[k][1] :
+                triangle = abs(points[i][1] - points[j][1]) * abs(points[j][0] - points[k][0])
+
+                max_traingle = max(max_traingle, triangle)
+
+print(max_traingle)
+
+
+
