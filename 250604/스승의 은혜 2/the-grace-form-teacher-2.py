159a20628af57c0eda5cb1d75c95055648123ed2
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-04 17:35:45 +0900

    Title: 스승의 은혜 2, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250604/스승의 은혜 2/the-grace-form-teacher-2.py
@@ -0,0 +1,42 @@
+N, B = map(int, input().split())
+
+price_list = list()
+
+for _ in range(N) :
+
+    price = int(input())
+    price_list.append(price)
+
+
+# 출력값
+max_students = float('-inf')
+
+
+for i in range(N) :
+
+    # 현재 선물 산 선물 가격의 총합, 선택되는 학생이 바뀔 때마다 초기화 해주어야 함. 
+    price_sum = 0
+
+    # 현재 선물을 받는 학생 수
+    cnt = 0
+
+    for j in range(N) :
+
+        if ( i == j ) :
+            # i -> 50퍼 할인 받는 학생
+            price_sum += (price_list[j] / 2) 
+        else :
+            price_sum += price_list[j]
+
+        # 범위 벗어나는 경우 break, 아닌 경우 학생 수 증가
+        if price_sum >= B :
+            break
+        else :
+            cnt += 1
+
+    max_students = max(max_students, cnt)
+
+print(max_students)
+
+        
+
