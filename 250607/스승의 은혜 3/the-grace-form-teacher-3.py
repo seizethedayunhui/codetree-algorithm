5746c64086a6c94217e2a3eae8a693f080bfe7d3
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-07 21:50:00 +0900

    Title: 스승의 은혜 3, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250607/스승의 은혜 3/the-grace-form-teacher-3.py
+++ 250607/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -7,6 +7,9 @@ for _ in range(N) :
     record = tuple(map(int, input().split()))
     records.append(record)
 
+records.sort( lambda x : x[0] + x[1] )
+
+
 max_cnt = float('-inf')
 for i in range(N) :
 
@@ -23,10 +26,20 @@ for i in range(N) :
 
         price_sum += records[j][1]
 
-        # 예산에 벗어난 경우 그냥 break
+        # print(f"{i} {j+1}번 {price_sum}")
+        # 예산 범위를 초과하는 경우 현재 것을 포함하지 않고 다음으로 넘어감.
+        # 현재는 배송비, 선물 값이 따로 주어진 것이기 때문에 다음 것의 총합이 더 작아지는 경우가 발생할 수 있음. 
         if B - price_sum < 0 :
-            break
-        # 예산에 딱 맞은 경우 cnt ++ 하고 break
+            price_sum -= records[j][1]
+
+            if i == j :
+                price_sum -= (records[j][0] // 2)
+            else :
+                price_sum -= records[j][0]
+
+            continue
+            
+        # 예산을 만족하는 경우
         elif B == price_sum :
             cnt += 1
             break
