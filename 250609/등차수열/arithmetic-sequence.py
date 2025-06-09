eabd632112ea054445ed7e84406dd98ac0fb0de6
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-09 20:09:13 +0900

    Title: 등차수열, Time: 56ms, Memory: 17MB, Status: Passed - Codetree

--- 250609/등차수열/arithmetic-sequence.py
+++ 250609/등차수열/arithmetic-sequence.py
@@ -3,15 +3,17 @@ N = int(input())
 num_list = list(map(int, input().split()))
 
 # 등차수열을 이루게 하는 것!
-cnt = 0
+max_cnt = float('-inf')
 for K in range(1, 101) :
-
+    current_cnt = 0
     for i in range(N) :
         for j in range(i+1, N) :
 
             if K - num_list[i] == num_list[j] - K :
-                cnt += 1
+                current_cnt += 1
+            
+    max_cnt = max(max_cnt, current_cnt)
 
-print(cnt)
+print(max_cnt)
 
            
