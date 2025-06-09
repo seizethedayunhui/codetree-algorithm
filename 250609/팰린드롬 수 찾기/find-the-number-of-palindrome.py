34688b467fd4c4315a78cb1a1d8c9eeed2ccae18
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-09 21:33:37 +0900

    Title: 팰린드롬 수 찾기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250609/팰린드롬 수 찾기/find-the-number-of-palindrome.py
@@ -0,0 +1,27 @@
+X, Y = map(int, input().split())
+
+def num_to_list(n) :
+
+    num_list = list()
+    while ( n != 0 ) :
+        num_list.append(n % 10)
+        n //= 10
+    
+    return num_list
+
+cnt = 0
+for n in range(X, Y) :
+
+    num_list = num_to_list(n)
+    flag = True
+
+    for i in range( len(num_list)//2 ) :
+
+        if num_list[i] != num_list[len(num_list)-1-i] :
+            flag = False
+            break
+    if flag :
+        cnt += 1
+
+print(cnt)
+
