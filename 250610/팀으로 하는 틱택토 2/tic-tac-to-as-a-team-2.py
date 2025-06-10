275cb049b426a1e094bb6abf5e28ccb359b9be7d
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-10 14:28:42 +0900

    Title: 팀으로 하는 틱택토 2, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250610/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
+++ 250610/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -52,7 +52,7 @@ def vertical(x, y, mat) :
     return digit_cnt
 
 # 대각선
-def diagonal(x, y, mat) :
+def diagonal1(x, y, mat) :
 
     dx = [0, 1, 2]
     dy = [0, 1, 2]
@@ -75,12 +75,35 @@ def diagonal(x, y, mat) :
                 digit_cnt += 1
 
     return digit_cnt
-        
+
+def diagonal2(x, y, mat) :
+
+    dx = [0, 1, 2]
+    dy = [0, -1, -2]
+
+    digits = [ 0 for _ in range(10) ]
+    flag = True
+
+    for i in range(3) :
+        nx, ny = x + dx[i], y + dy[i]
+        if in_range(nx, ny) :
+            digits[mat[nx][ny]] += 1
+        else :
+            flag = False
+            break
+
+    digit_cnt = 0
+    if flag :
+        for digit in digits :
+            if digit :
+                digit_cnt += 1
+
+    return digit_cnt      
             
 cnt = 0
 for i in range(3) :
     for j in range(3) :
-        if horizontal(i, j, mat) == 2 or vertical(i, j, mat) == 2 or diagonal(i, j, mat) == 2 :
+        if horizontal(i, j, mat) == 2 or vertical(i, j, mat) == 2 or diagonal1(i, j, mat) == 2 or diagonal2(i, j, mat) == 2:
             cnt += 1
 
 print(cnt)
