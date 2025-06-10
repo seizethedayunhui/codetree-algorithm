58b136a204a3248726887408f8d9df1a6d8cd34a
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-10 14:26:10 +0900

    Title: 팀으로 하는 틱택토 2, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250610/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.py
@@ -0,0 +1,88 @@
+mat = [ [ int(elem) for elem in input() ] for _ in range(3) ]
+
+def in_range(x, y) :
+    return 0 <= x < 3 and 0 <= y < 3
+
+# 가로
+def horizontal(x, y, mat) :
+
+    dx = [0, 1, 2]
+
+    digits = [ 0 for _ in range(10) ]
+    flag = True
+
+    for i in range(3) :
+        nx, ny = x + dx[i], y
+        if in_range(nx, ny) :
+            digits[mat[nx][ny]] += 1
+        else :
+            flag = False
+            break
+
+    digit_cnt = 0
+    if flag :
+        for digit in digits :
+            if digit :
+                digit_cnt += 1
+
+    return digit_cnt
+
+# 세로
+def vertical(x, y, mat) :
+
+    dy = [0, 1, 2]
+
+    digits = [ 0 for _ in range(10) ]
+    flag = True
+
+    for i in range(3) :
+        nx, ny = x , y + dy[i]
+        if in_range(nx, ny) :
+            digits[mat[nx][ny]] += 1
+        else :
+            flag = False
+            break
+
+    digit_cnt = 0
+    if flag :
+        for digit in digits :
+            if digit :
+                digit_cnt += 1
+
+    return digit_cnt
+
+# 대각선
+def diagonal(x, y, mat) :
+
+    dx = [0, 1, 2]
+    dy = [0, 1, 2]
+
+    digits = [ 0 for _ in range(10) ]
+    flag = True
+
+    for i in range(3) :
+        nx, ny = x + dx[i], y + dy[i]
+        if in_range(nx, ny) :
+            digits[mat[nx][ny]] += 1
+        else :
+            flag = False
+            break
+
+    digit_cnt = 0
+    if flag :
+        for digit in digits :
+            if digit :
+                digit_cnt += 1
+
+    return digit_cnt
+        
+            
+cnt = 0
+for i in range(3) :
+    for j in range(3) :
+        if horizontal(i, j, mat) == 2 or vertical(i, j, mat) == 2 or diagonal(i, j, mat) == 2 :
+            cnt += 1
+
+print(cnt)
+    
+
