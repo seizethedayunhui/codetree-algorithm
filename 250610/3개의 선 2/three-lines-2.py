f5e6cd0de0604b845c0e32704f6024845b43fd1d
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-10 13:40:08 +0900

    Title: 3개의 선 2, Time: 96ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250610/3개의 선 2/three-lines-2.py
@@ -0,0 +1,62 @@
+N = int(input())
+points = list()
+
+for _ in range(N) :
+    point = tuple(map(int, input().split()))
+    points.append(point)
+
+cnt = 0
+for i in range(11) :
+    for j in range(11) :
+        for k in range(11) :
+            
+            # 같은 것이 있는 경우 break
+            if ( i == j ) or ( j == k ) or ( k == i ) :
+                continue
+
+            # 1. 모두 x축에 평행한 경우
+            cnt1 = 0
+            flag1 = False
+            for point in points:
+                if point[1] == i or point[1] == j or point[1] == k :
+                    cnt1 += 1
+            if cnt1 == N :
+                flag1 = True
+
+            # 2. 모두 y축에 평행한 경우
+            cnt2 = 0
+            flag2 = False
+            for point in points :
+                if point[0] == i or point[0] == j or point[0] == k :
+                    cnt2 += 1
+            if cnt2 == N :
+                flag2 = True
+
+            # 3. x축에 평행한 것 1개, y축에 평행한 것 2개
+            cnt3 = 0
+            flag3 = False
+            for point in points :
+                if point[1] == i or point[0] == j or point[0] == k :
+                    cnt3 += 1
+            if cnt3 == N :
+                flag3 = True
+
+            # 4. y축에 평행한 것 1개, x축에 평행한 것 2개
+            cnt4 = 0
+            flag4 = False
+            for point in points :
+                if point[0] == i or point[1] == j or point[1] == k :
+                    cnt4 += 1
+            if cnt4 == N :
+                flag4 = True
+
+            if flag1 or flag2 or flag3 or flag4 :
+                cnt += 1
+
+if cnt :
+    print(1)
+else :
+    print(0)
+
+            
+            
