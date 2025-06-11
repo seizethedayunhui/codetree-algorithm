9961348a25a0851cfa490cc65b3c36141dd113b4
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-11 23:37:12 +0900

    Title: 수들의 최대 차, Time: 89ms, Memory: 17MB, Status: Passed - Codetree

--- 250611/수들의 최대 차/maximum-difference-in-numbers.py
+++ 250611/수들의 최대 차/maximum-difference-in-numbers.py
@@ -8,12 +8,11 @@ max_cnt = float('-inf')
 for i in range(N) :
     # i == 최솟값의 인덱스
     current_cnt = 0
-    
-    for j in range(N) :
-        if i == j :
-            continue
 
-        elif abs(num_list[i]-num_list[j]) <= K :
+    for j in range(N) :
+
+        # i를 최솟값의 인덱스라고 잡았기 때문에 i 인덱스에 해당하는 값이 j 인덱스에 해당하는 값보다 큰지 확인해야함. 
+        if num_list[j] >= num_list[i] and num_list[j]-num_list[i] <= K :
             current_cnt += 1
 
     max_cnt = max(max_cnt, current_cnt)
