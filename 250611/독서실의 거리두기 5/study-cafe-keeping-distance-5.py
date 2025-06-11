3d952f9cb136746b0107ff82a92430d900655345
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-11 17:51:38 +0900

    Title: 독서실의 거리두기 5, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250611/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
+++ 250611/독서실의 거리두기 5/study-cafe-keeping-distance-5.py
@@ -10,19 +10,26 @@ for i in range(N) :
     if records[i] :
         continue
 
+    # 해당 위체 학생을 넣는 경우 가정 -> 원소 값을 1로 변경
+    records[i] = 1
     current_min_dis = float('inf')
 
-    # 가장 가까이에 있는 학생들의 거리를 구함
-    for k in range(N) :
-        if k == i :
-            continue
+    # 각 원소들과의 가장 가까운 거리를 구하는 과정
+    for j in range(N) :
 
-        if records[k] :
-            current_dis = abs(i-k)
-            current_min_dis = min(current_min_dis, current_dis)
+        if records[j] :
+
+            for k in range(j+1, N) :
+                if records[k] :
+                    current_dis = abs(k-j)
+                    current_min_dis = min(current_min_dis, current_dis)
+            
 
     # 가장 가까이에 있는 학생들의 거리 중 가장 멀게 있는 것을 선택함. 
     max_dis = max(max_dis, current_min_dis)
+
+    # 다시 값을 초기화
+    records[i] = 0
 
 print(max_dis)
 
