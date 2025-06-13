9db710438a88f5a1b11aa1c9a5df5f7eef4a8c16
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-13 17:44:31 +0900

    Title: 구간 잘 나누기, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250613/구간 잘 나누기/divide-sections-well.py
+++ 250613/구간 잘 나누기/divide-sections-well.py
@@ -1,62 +1,44 @@
-N, M = map(int, input().split())
+MAX_A = 10000
 
-arr = list(map(int, input().split()))
+#변수 선언 및 입력
+n, m = tuple(map(int, input().split()))
 
-def combinations(idx, pick, picks, M, N) :
+a = list(map(int, input().split()))
 
-    if len(pick) == M - 1 :
-        # 값은 복사해서 넣어주어야 함.
-        # 리스트는 이후에 계속 값이 바뀌기 때문에
-        picks.append(pick[:])
-        return
+# 100개의 숫자를 여러 개의 구간으로 나누는 경우의 수는 굉장히 많습니다.
+# 숫자들을 모든 구간으로 나누어 완전탐색하기 위해서는
+# 최악의 경우 99C50 = 약 5 * 10^28개의 경우를 세야 합니다.
+# 이는 아무리 컴퓨터라 해도 너무 많은 경우의 수를 세야 합니다.
+# 따라서 보다 효율적인 방법을 찾아야 합니다.
 
-    if idx >= N - 1 :
-        return
-    
+# 잘 생각해 보면, 구간의 합의 최댓값을 결정한다면
+# 구간을 몇 개로 나눠야 하는지 손쉽게 찾을 수 있습니다.
+ans = MAX_A
+for i in range(1, MAX_A + 1):
+    # 구간의 합의 최댓값이 i일 때
 
-    # 재귀 탐색
-    pick.append(idx)
-    # 넣는 경우
-    combinations(idx + 1, pick, picks, M, N)
+    # possible : 구간을 나눌 수 있으면 true
+    # section : 나뉘어져야 하는 구간의 개수
+    possible = True
+    section = 1
 
-    # 넣지 않는 경우
-    pick.pop()
-    combinations(idx + 1, pick, picks, M, N)
-
-pick = []
-picks = []
-
-# 재귀 탐색으로 가능한 조합수를 모두 구함
-combinations(0, pick, picks, M, N)
-
-min_max_sum = float('inf')
-for pick in picks :
-
-    current_max_sum = float('-inf')
-    start = 0
-
-    for elem in pick :
-        current_sum = 0
-        for i in range(start, elem+1):
-            current_sum += arr[i]
-        start = elem + 1
-        current_max_sum = max(current_max_sum, current_sum)
-
-    current_sum = 0
-    for i in range(start, N):
-        current_sum += arr[i]
-    current_max_sum = max(current_max_sum, current_sum)
-    
-    min_max_sum = min(min_max_sum, current_max_sum)
-
-print(min_max_sum)
+    cnt = 0
+    for j in range(n):
+        # 숫자 하나가 i보다 크면 구간을 나눌 수 없습니다.
+        if a[j] > i:
+            possible = False
+            break
         
+        # j번째 숫자가 들어갔을 때 i보다 커지면
+        # j번째 숫자부터 다음 구간으로 만듭니다.
+        if cnt + a[j] > i:
+            cnt = 0
+            section += 1
 
+        # 이번 구간에 j번째 숫자를 집어넣습니다.
+        cnt += a[j]
 
-        
+    if possible and section <= m:
+        ans = min(ans, i)
 
-
-
-
-    
-    
+print(ans)
