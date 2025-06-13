55c92d3c0cb33d5aa2339dc84589fd80fa75a77f
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-13 13:58:16 +0900

    Title: 이상한 폭탄 3, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250613/이상한 폭탄 3/strange-bomb-3.java
+++ 250613/이상한 폭탄 3/strange-bomb-3.java
@@ -17,35 +17,58 @@ public class Main {
         }
 
         // 큰값을 구하기 위한 기본값
-        double max_bomb_num = Double.NEGATIVE_INFINITY;
-        double max_cnt = Double.NEGATIVE_INFINITY;
+        int maxBombNum = 0;
+        double maxCnt = Double.NEGATIVE_INFINITY;
+        boolean flag = false;
+
+        // 전체 터진 것을 메모하는 배열
+        int[] records = new int[N];
 
         // j = 기준이 되는 인덱스
         for(int j = 0; j < N ; j++ ){
 
-            // bombs 배열을 전부 돌면서 같은 정수의 폭탄이 K 이내에 있는지 확인
-            boolean flag = false;
-            int cnt = 0;
+            // 터지는 경우를 메모
             for (int k = 0; k < N ; k++){
 
                 if (bombs[j]==bombs[k] && Math.abs(j-k) <= K){
                     flag = true;
-                    cnt += 1;
+                    // 터진 것을 기록함. 
+                    records[k] = 1;
                 }
             }
 
-            // flag와 횟수 비교
-            if (flag && cnt > max_cnt ) {
-                max_bomb_num = bombs[j];
-                max_cnt = cnt;
-            } else if ( cnt == max_cnt ){
-                max_bomb_num = Math.max(max_bomb_num, bombs[j]);
-                max_cnt = cnt;
-            }
-   
+                
         }
 
-        System.out.print((int)max_bomb_num);
+
+        if (flag){
+
+            for (int l = 0; l < N ; l++){
+
+                if (records[l] == 1) {
+                    int cnt = 0;
+                    for (int m = 0; m < N ; m++){
+                        if ( records[m] == 1 && bombs[l] == bombs[m] ){
+                                cnt += 1;
+                        
+                    }
+                    // System.out.println(bombs[l] + "일 때 cnt : " + cnt + ", 현재 최댓값: " + maxCnt );
+                    if (cnt > maxCnt ) {
+                        maxBombNum = bombs[l];
+                        maxCnt = cnt;
+                    } else if ( cnt == maxCnt ){
+                            maxBombNum = Math.max(maxBombNum, bombs[l]);
+                            maxCnt = cnt;
+                    }
+
+                        
+                    }
+                }
+                
+            }
+
+        System.out.print(maxBombNum);
         
+        }
     }
 }
