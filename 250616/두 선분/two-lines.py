52926e5bd6fed78dd1281709e1f371384350f7e9
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-16 20:50:42 +0900

    Title: 두 선분, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250616/두 선분/two-lines.py
+++ 250616/두 선분/two-lines.py
@@ -2,7 +2,7 @@ x1, x2, x3, x4 = map(int, input().split())
 
 flag = False
 
-if (x3 >= x1 or x3 <= x2) or (x4 >= x1 or x4 <= x2) or (x1 >= x3 or x1 <= x4) or (x2 >= x3 or x2 <= x4) :
+if (x3 >= x1 and x3 <= x2) or (x4 >= x1 and x4 <= x2) or (x1 >= x3 and x1 <= x4) or (x2 >= x3 and x2 <= x4) :
     flag = True
 
 if flag :
