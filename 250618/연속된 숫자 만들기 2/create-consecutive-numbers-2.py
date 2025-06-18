240395e58e67325cfe70084fa885c43f2f02d9d8
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-18 17:50:08 +0900

    Title: 연속된 숫자 만들기 2, Time: 1074ms, Memory: 16MB, Status: Time Limit Exceed - Codetree

--- 250618/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
+++ 250618/연속된 숫자 만들기 2/create-consecutive-numbers-2.py
@@ -1,25 +1,60 @@
 location = list(map(int, input().split()))
-location.sort() # 최소 위치..
+location.sort() 
 
-gap = 0
 cnt = 0
+gap = float('inf')
 
 while (gap != 2) :
 
-    if (location[1] - location[0] > location[2] - location[1]) :
+    gap1 = location[1] - location[0]
+    gap2 = location[2] - location[1]
+
+    if (gap1 == 1) :
 
         temp = location[1]
-        location[1] = (temp + location[0]) // 2 
+
+        if (((location[2] - location[1]) % 2)) :
+            location[1] = (temp + location[2]) // 2 + 1
+        else :
+            location[1] = (temp + location[2]) // 2
+        
+        location[0] = temp
+
+    elif (gap1 < gap2) :
+
+        temp = location[1]
+
+        if (((location[1] - location[0]) % 2)) :
+            location[1] = (temp + location[0]) // 2 + 1
+        else :
+            location[1] = (temp + location[0]) // 2
+            
+        location[2] = temp
+
+    elif (gap2 == 1) :
+
+        temp = location[1]
+
+        if (((location[1] - location[0]) % 2)) :
+            location[1] = (temp + location[0]) // 2 + 1
+        else :
+            location[1] = (temp + location[0]) // 2
+            
         location[2] = temp
 
     else :
-
         temp = location[1]
-        location[1] = (temp + location[2]) // 2 
+
+        if (((location[2] - location[1]) % 2)) :
+            location[1] = (temp + location[2]) // 2 + 1
+        else :
+            location[1] = (temp + location[2]) // 2
+        
         location[0] = temp
 
     gap = abs(location[1]-location[0]) + abs(location[2]-location[1])
     cnt += 1
+    # print(location)
 
 print(cnt)
 
