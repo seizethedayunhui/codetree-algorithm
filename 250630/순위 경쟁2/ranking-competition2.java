1c8d50aed9ca4e148f4eb41c3f4d2ade7ef783f2
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-30 21:27:51 +0900

    Title: 순위 경쟁2, Time: 138ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250630/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        String currentWinner = "None";
+
+        int scoreA = 0;
+        int scoreB = 0;
+
+        int cnt = 0;
+
+        for(int i = 0; i < N; i++){
+            String who = sc.next();
+            int change = sc.nextInt();
+
+            if (who.equals("A")){
+                scoreA += change;
+            } else {
+                scoreB += change;
+            }
+
+            if (currentWinner.equals("None")){
+
+                if (scoreA > scoreB){
+                    currentWinner = "A";
+                } else if (scoreA == scoreB) {
+                    currentWinner = "Both";
+                } else {
+                    currentWinner = "B";
+                }
+
+                cnt += 1;
+
+            } else if ( currentWinner.equals("Both")){
+
+                if (scoreA > scoreB){
+                    currentWinner = "A";
+                    cnt += 1;
+                } else if (scoreA < scoreB) {
+                    currentWinner = "B";
+                    cnt += 1;
+                } 
+
+            } else if ( currentWinner.equals("A")){
+
+                if (scoreA == scoreB) {
+                    currentWinner = "Both";
+                    cnt += 1;
+                } else if (scoreA < scoreB) {
+                    currentWinner = "B";
+                    cnt += 1;
+                }
+                
+            } else if ( currentWinner.equals("B")){
+
+                if (scoreA > scoreB){
+                    currentWinner = "A";
+                    cnt += 1;
+                } else if (scoreA == scoreB) {
+                    currentWinner = "Both";
+                    cnt += 1;
+                } 
+
+            }
+        }
+
+        System.out.println(cnt);
+    }
+}
