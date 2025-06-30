b38a47183336973aa931618c71fb87219117b7b7
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-30 21:14:57 +0900

    Title: ABC 줄 세우기, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250630/ABC 줄 세우기/abc-line-up.py
+++ 250630/ABC 줄 세우기/abc-line-up.py
@@ -4,15 +4,32 @@ arr = list(map(ord, input().split()))
 
 cnt = 0
 
-for i in range(1, N) :
+# 처음부터 끝까지 loop
+for i in range(N) :
 
-    if (arr[i] < arr[i-1]) :
+    # 최소 change 횟수를 찾아야함
+    # 따라서, 정렬되지 않는 부분의 min_num과 그 idx를 저장
+    min_num = arr[i]
+    point = i
 
-        temp = arr[i-1]
-        arr[i-1] = arr[i]
-        arr[i] = temp
+    # min_num이랑 위치 찾음.
+    for j in range(i+1, N) :
 
-        cnt += 1
+        if arr[j] < min_num :
+            min_num = arr[j]
+            point = j
+
+    # 현재 수가 정렬되지 않은 부분에서 min_num이 아닌 경우
+    if point != i :
+
+        # 카운팅을 하기 위해서 버블정렬 사용
+        for k in range(point, i, -1) :
+
+            cnt += 1 # 카운트
+
+            temp = arr[k]
+            arr[k] = arr[k-1]
+            arr[k-1] = temp
 
 print(cnt)
 
