284bbf275eddce6bb67655bb7e7fc8fc4fd1e844
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-06-30 20:32:55 +0900

    Title: 연속된 숫자 만들기 3, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250630/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
+++ 250630/연속된 숫자 만들기 3/create-consecutive-numbers-3.py
@@ -3,27 +3,29 @@ s1, s2, s3 = map(int, input().split())
 gap1 = abs(s2 - s1)
 gap2 = abs(s3 - s2)
 
-cnt = 0
+cnt = max(gap1, gap2) - 1
 
-while not (gap1 == 1 and gap2 == 1) :
+# while not (gap1 == 1 and gap2 == 1) :
 
-    if gap1 >= gap2 :
+#     if gap1 > gap2 :
 
-        temp = s2
+#         temp = s2
+#         s2 = s2 - 1
+#         s3 = temp
+   
+#     else :
 
-        s2 = s1 + 1
-        s3 = temp
+#         temp = s2
 
-    else :
+#         s2 = s3 - 1
+#         s1 = temp
 
-        temp = s2
+#     gap1 = abs(s2 - s1)
+#     gap2 = abs(s3 - s2)
 
-        s2 = temp + 1
-        s1 = temp
+#     cnt += 1
 
-    gap1 = abs(s2 - s1)
-    gap2 = abs(s3 - s2)
-
-    cnt += 1
+#     print(s1, s2, s3)
+#     print(gap1, gap2)
 
 print(cnt)
