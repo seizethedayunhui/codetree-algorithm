da9b10fa0184b410eec154c01b2d34132df17a17
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-07-21 23:22:38 +0900

    Title: 최소 와이파이 수, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250721/최소 와이파이 수/minimum-number-of-wifi.py
@@ -4,21 +4,21 @@ arr = list(map(int, input().split()))
 
 start_point = 0
 cnt = 0
-not_available = True
+
 
 while (start_point < n) :
 
+    not_available = True
     # 완벽한 스타트 포인트 결정
     for i in range(start_point, n) :
         if arr[i] :
             not_available = False
             start_point = i
             break
-    
-    if not_available :
-        break
 
-    cnt += 1
+    # 해당 값이 0인 경우 사람이 살지 않기 때문에 굳이 와이파이를 설치할 필요가 없음. 
+    if not not_available :
+        cnt += 1
 
     next_point = start_point + (2 * m  + 1)
     start_point = next_point
