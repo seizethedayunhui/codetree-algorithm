95a7738d7ce057cdb61fcacede03c289a6f34d1e
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-07-21 23:18:39 +0900

    Title: 최소 와이파이 수, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250721/최소 와이파이 수/minimum-number-of-wifi.py
+++ 250721/최소 와이파이 수/minimum-number-of-wifi.py
@@ -4,14 +4,19 @@ arr = list(map(int, input().split()))
 
 start_point = 0
 cnt = 0
+not_available = True
 
 while (start_point < n) :
 
     # 완벽한 스타트 포인트 결정
     for i in range(start_point, n) :
         if arr[i] :
+            not_available = False
             start_point = i
             break
+    
+    if not_available :
+        break
 
     cnt += 1
 
