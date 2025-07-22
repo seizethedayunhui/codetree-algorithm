ecfa0aba075a1d2676e54c711a010e4bb877421d
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-07-22 23:29:00 +0900

    Title: 홀수 짝수의 묶음, Time: 72ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250722/홀수 짝수의 묶음/odd-even-bundle.py
+++ 250722/홀수 짝수의 묶음/odd-even-bundle.py
@@ -31,11 +31,13 @@ for i in range(N) :
     # print(i)
     # 홀수가 와야하는 경우
     if i % 2 :
+        # 짝수를 다 사용한경ㅇ + 원래 홀수의 개수가 홀수인 경우 + 남은 홀수의 개수가 홀수인 경우
         if even_cnt >= len(even_list) and (len(odd_list) - odd_cnt) % 2 and len(odd_list) % 2 :
                 for _ in range(3) :
                     current_list.append(odd_list[odd_cnt])
                     odd_cnt += 1
                 cnt += 1
+        # 보통의 경우
         else :
             cnt += 1
             current_list.append(odd_list[odd_cnt])
@@ -48,9 +50,14 @@ for i in range(N) :
                 current_list.append(odd_list[odd_cnt])
                 odd_cnt += 1
             cnt += 1
-
+        # 홀수를 다 사용하고 짝수만 남은 경우
+        elif odd_cnt >= len(odd_list) :
+            for j in range(even_cnt, len(even_list)) :
+                current_list.append(even_list[j])
+                even_cnt += 1
+            cnt += 1
+        # 보통의 경우 - 짝수 위치에는 짝수 하나만 있도록 함. 
         else :
-            # 짝수 위치에는 짝수 하나만 있도록 함. 
             cnt += 1
             current_list.append(even_list[even_cnt])
             even_cnt += 1
