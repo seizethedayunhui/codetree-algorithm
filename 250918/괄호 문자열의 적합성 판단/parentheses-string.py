bfbea06e9a839c30b2330a4cd1a2a24cf3ed7054
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-09-18 22:13:28 +0900

    Title: 괄호 문자열의 적합성 판단, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250918/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 250918/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,13 +1,32 @@
 input_str = input()
 
 idx = 0
-for element in input_str :
-    if element == "(" :
-        idx += 1
-    elif idx :
-        idx -= 1
 
-if not idx :
+stack = [""] * 50
+is_empty = True
+flag = True
+
+for element in input_str :
+
+    if element == "(" :
+        if is_empty :
+            is_empty = False
+        idx += 1
+
+    elif element == ")" :
+        if is_empty :
+            flag = False
+            break
+        else :
+            idx -= 1
+        
+        if not idx :
+            is_empty = True
+
+if is_empty and flag :
     print("Yes")
 else :
     print("No")
+        
+
+        
