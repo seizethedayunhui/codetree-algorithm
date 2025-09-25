280b161dbec4e26dbf97baa9d3e60d997fbdda7b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-09-25 21:29:41 +0900

    Title: 정수 명령 처리 3, Time: 360ms, Memory: 22MB, Status: Wrong Answer - Codetree

---
+++ 250925/정수 명령 처리 3/process-numeric-commands-3.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        String command;
+        Deque<Integer> dq = new ArrayDeque<>();
+        for(int i = 0; i < N; i++){
+            command = sc.next();
+            if (command.equals("push_front")){
+                dq.addFirst(sc.nextInt()); 
+            } else if (command.equals("push_back")){
+                dq.addLast(sc.nextInt());
+            } else if (command.equals("pop_front")){
+                System.out.println(dq.pollFirst());
+            } else if (command.equals("pop_back")){
+                System.out.println(dq.pollLast());
+            } else if (command.equals("size")){
+                System.out.println(dq.size());
+            } else if (command.equals("empty")){
+                int ans = (command.isEmpty()) ? 1 : 0;
+                System.out.println(ans);
+            } else if (command.equals("front")){
+                System.out.println(dq.peekFirst());
+            } else {
+                System.out.println(dq.peekLast());
+            }
+        }
+    }
+}
