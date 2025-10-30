c796763e2c2c23a918bff85759ee4556161a2905
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-10-30 23:26:58 +0900

    Title: 트로미노, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 251030/트로미노/tromino.py
@@ -0,0 +1,47 @@
+N, M = map(int, input().split())
+
+mat = list()
+for _ in range(N) :
+    row = list(map(int, input().split()))
+    mat.append(row)
+
+maxSum = float('-inf')
+
+# 3 * 1
+for i in range(N-2) :
+    for j in range(N) :
+        currentSum = 0
+        for k in range(3) :
+            currentSum += mat[i+k][j]
+        maxSum = max(maxSum, currentSum)
+
+# 1 * 3
+for i in range(N-2) :
+    for j in range(N) :
+        currentSum = 0
+        for k in range(3) :
+            currentSum += mat[j][i+k]
+        maxSum = max(maxSum, currentSum) 
+
+dx = [ [ 0, 1, 1], [ 0, 0, 1], [ 0, 0, 1], [ 0, 1, 1] ]
+dy = [ [ 0, 0, 1], [ 0, 1, 0], [ 0, 1, 1], [ 1, 1, 0] ]
+
+
+# 계단 모양
+for i in range(N-1):
+    for j in range(N-1):   
+        # 하나의 지점에 도착
+        # 4가지 경우의 수
+        for k in range(4):
+            currentSum = 0
+            for l in range(3):
+                currentSum += mat[i + dx[k][l]][j + dy[k][l]]
+            maxSum = max(maxSum, currentSum)
+
+print(maxSum)
+
+
+        
+        
+
+       
