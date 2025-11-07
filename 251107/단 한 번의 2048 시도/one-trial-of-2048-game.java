17529b4177197a866830b4458cab39d3c8a8f00a
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-11-07 23:09:54 +0900

    Title: 단 한 번의 2048 시도, Time: 124ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251107/단 한 번의 2048 시도/one-trial-of-2048-game.java
+++ 251107/단 한 번의 2048 시도/one-trial-of-2048-game.java
@@ -52,13 +52,18 @@ public class Main {
             }
             for(int row = 0; row < 4; row++){
                 for(int i = startIdx; i != endIdx; ){
-                    if(temp[row][i]==0){
-                        temp[row][i] = temp[row][i+d];
-                        temp[row][i+d] = 0;
-                    }
 
                     if(temp[row][i] == temp[row][i+d]){
                         temp[row][i] += temp[row][i+d];
+                        temp[row][i+d] = 0;
+                    }
+                    i += d;
+                }
+            }
+            for(int row = 0; row < 4; row++){
+                for(int i = startIdx; i != endIdx; ){
+                    if(temp[row][i]==0){
+                        temp[row][i] = temp[row][i+d];
                         temp[row][i+d] = 0;
                     }
                     i += d;
@@ -81,15 +86,22 @@ public class Main {
 
             for(int col = 0; col < 4; col++){
                 for(int j = startIdx; j != endIdx; ){
-                    if(temp[j][col]==0){
-                        temp[j][col] = temp[j+d][col];
-                        temp[j+d][col] = 0;
-                    }
 
                     if (temp[j][col] == temp[j+d][col]){
                         temp[j][col] += temp[j+d][col];
                         temp[j+d][col] = 0;
                     }
+                    j += d;
+                }
+            }
+            
+            for(int col = 0; col < 4; col++){
+                for(int j = startIdx; j != endIdx; ){
+                    if(temp[j][col]==0){
+                        temp[j][col] = temp[j+d][col];
+                        temp[j+d][col] = 0;
+                    }
+
                     j += d;
                 }
             }
