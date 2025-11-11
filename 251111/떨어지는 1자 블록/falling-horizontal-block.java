4832e74ee968abf7dde09a392eb524e3b62870d4
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-11-11 23:15:28 +0900

    Title: 떨어지는 1자 블록, Time: 169ms, Memory: 11MB, Status: Passed - Codetree

--- 251111/떨어지는 1자 블록/falling-horizontal-block.java
+++ 251111/떨어지는 1자 블록/falling-horizontal-block.java
@@ -17,22 +17,31 @@ public class Main {
         }
 
         boolean flag;
-        for(int x = N-1; x > -1; x--){
+        boolean findflag = false;
+        int r = -1;
+
+        for(int x = 0; x < N; x++){
             flag = true;
+
             for(int y = K; y < K + M; y++){
                 if (mat[x][y] != 0){
                     flag = false;
+                    findflag = true;
                     break;
                 }
             }
 
             if (flag){
-                for(int y = K; y < K + M; y++){
-                    mat[x][y] = 1;
-                }
-                
+                r = x;
+            }
+
+            if (findflag){
                 break;
             }
+        }
+        // flag가 true인 경우에 row 행을 1로 갱신 해줌
+        for(int k = K; k < K + M; k++){
+            mat[r][k] = 1;
         }
 
         for (int row = 0; row < N; row++){
