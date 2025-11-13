e88c5ea69920c425d61b3301cac12d43ce828cea
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-11-13 22:40:21 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 251113/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
+++ 251113/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.py
@@ -13,8 +13,30 @@ for _ in range(N) :
         element_list.append(e)
     mat.append(element_list)
 
+# 범위 안에 있는지 확인하는 함수
 def in_range(x, y) :
     return 0 <= x < N and 0 <= y < N
+
+# 사방이 막혔는지 확인하는 함수 
+def is_possible(x, y, direc) :
+
+    right_direc = (direc + 1) % 4
+    left_direc = (direc + 3) % 4
+
+    nx = x + dx[direc]
+    ny = y + dy[direc]
+
+    right_nx = x + dx[right_direc]
+    right_ny = y + dy[right_direc]
+
+    left_nx = x + dx[left_direc]
+    left_ny = y + dy[left_direc]
+
+    if mat[left_nx][left_ny] == "#" and mat[right_nx][right_ny] == "#" and mat[nx][ny] == "#":
+        return False
+    
+    return True
+
 
 dx = [ 0, 1, 0, -1]
 dy = [ 1, 0, -1, 0]
@@ -62,12 +84,17 @@ while in_range(nx, ny):
             ny += dy[direc]
 
     else :
+
+        if not is_possible(nx, ny, direc) :
+            flag = False
+            break  
         direc = (direc + 3) % 4
 
     # 처음의 위치로 돌아온 경우 탈출할 수 없다는 의미이므로 break
     if not direc and (nx == x and ny == y) :
         flag = False
         break
+  
 
 
 
