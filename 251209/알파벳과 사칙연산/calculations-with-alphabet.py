dd0f6822279eab5ce0a0496ae26eab0f1b153da8
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-12-09 16:37:54 +0900

    Title: 알파벳과 사칙연산, Time: 85ms, Memory: 19MB, Status: Passed - Codetree

--- 251209/알파벳과 사칙연산/calculations-with-alphabet.py
+++ 251209/알파벳과 사칙연산/calculations-with-alphabet.py
@@ -2,6 +2,8 @@ equation = input()
 
 operations = list()
 elements = list()
+element_list = list()
+element_dict = {}
 
 N = len(equation)
 
@@ -10,45 +12,51 @@ for e in equation :
         operations.append(e)
     else :
         elements.append(e)
+        if not e in element_list :
+            element_list.append(e)
 
-
-M = len(elements)
+M = len(element_list)
 
 
 def calc_equation() :
 
     calc_current = 0
 
-    for i in range(M) :
+    for i in range(len(elements)) :
+        element_num = element_dict[elements[i]]
         # 연산기호
         if i :
             if operations[i-1] == "*" :
-                calc_current *= elements[i]
+                calc_current *= element_num
             elif operations[i-1] == "+" :
-                calc_current += elements[i]
+                calc_current += element_num
             else :
-                calc_current -= elements[i]
+                calc_current -= element_num
         else :
-            calc_current = elements[i]
+            calc_current = element_num
+        # print(calc_current)
     return calc_current
 
 
 
 def complete_equation(idx) :
-
     
     if idx == M :
-        return calc_equation()
+        cur = calc_equation()
+        # print(element_dict)
+        # print(element_list)
+        # print(cur)
+        return cur
 
     max_ans = float('-inf')
 
     for i in range(4) :
-        elements[idx] = (i+1)
+        e = element_list[idx]
+        element_dict[e] = (i+1)
         max_ans = max(max_ans, complete_equation(idx+1))
 
     return max_ans
 
     
-
 ans = complete_equation(0)
 print(ans)
