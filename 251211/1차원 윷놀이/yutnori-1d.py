9086abca208905cdbdedec21db088753cfa3a312
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-12-11 15:06:40 +0900

    Title: 1차원 윷놀이, Time: 3410ms, Memory: 25MB, Status: Passed - Codetree

--- 251211/1차원 윷놀이/yutnori-1d.py
+++ 251211/1차원 윷놀이/yutnori-1d.py
@@ -3,7 +3,6 @@ N, M, K = map(int, input().split())
 moves = list(map(int, input().split()))
 
 positions_arr = [ 0 for _ in range(K) ]
-#prev_positions_arr = [ 0 for _ in range(K) ]
 points_arr = [ 0 for _ in range(K) ]
 
 def cal_points(points_arr) :
@@ -17,10 +16,6 @@ def get_max_points(idx) :
 
     if idx == N :
         check = cal_points(points_arr)
-        # print(points_arr)
-        # print(positions_arr)
-        # print(check)
-        # print()
         return check
 
     max_points = 0
@@ -28,17 +23,17 @@ def get_max_points(idx) :
         # 말의 이동로직
         next_position = moves[idx] + positions_arr[i]
 
+        # 처음으로 M 위치에 도착한 경우
         if next_position >= M-1 and positions_arr[i] < M-1 :
             points_arr[i] = 1
 
-        #prev_positions_arr[i] = positions_arr[i]
         positions_arr[i] = next_position
         
-        #print(idx, "번째: ", i, positions_arr)
         max_points = max(max_points, get_max_points(idx+1))
 
         positions_arr[i] -= moves[idx]
-        # print("벗어남", positions_arr)
+
+        # 처음으로 M위치에 도달한 경우 원래대로 되돌려 주어야함. 
         if points_arr[i] and positions_arr[i] < M-1:
             points_arr[i] = 0
     
