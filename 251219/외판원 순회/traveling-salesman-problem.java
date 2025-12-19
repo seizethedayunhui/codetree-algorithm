9ecd97e3e7a947baaa70100fd9ef1128780c2d70
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2025-12-19 15:26:48 +0900

    Title: 외판원 순회, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251219/외판원 순회/traveling-salesman-problem.java
+++ 251219/외판원 순회/traveling-salesman-problem.java
@@ -9,7 +9,8 @@ public class Main {
     public static int findMinSum(int currentSum, int location, int cnt){
         
         if (cnt == N-1){
-            // System.out.println("들어옴");
+            int c = currentSum + mat[location][0];
+            // System.out.println("들어옴" + ", " + c);
             return currentSum + mat[location][0];
         }
 
@@ -17,7 +18,7 @@ public class Main {
 
         for(int i = 1; i < N; i++){
 
-            if(visited[i]){
+            if(visited[i] || location == i || mat[location][i] == 0){
                 continue;
             }
 
