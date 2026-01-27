84ff07e56561f1d5959949a1ee0bd2e16837e08b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-01-28 01:54:09 +0900

    Title: 계단 오르기, Time: 50ms, Memory: 17MB, Status: Wrong Answer - Codetree

---
+++ 260128/계단 오르기/climbing-stairs.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+dp = [-1] * (N+1)
+
+def step(n):
+
+    if dp[n] != -1:
+        return dp[n]
+    
+    elif n <= 1:
+        if n == 0:
+            dp[n] = 1
+        else:
+            dp[n] = 0
+    
+    else:
+        dp[n] = step(n-2) + step(n-3)
+    
+    return dp[n]
+
+ans = step(N)
+print(ans)
+    
+    
