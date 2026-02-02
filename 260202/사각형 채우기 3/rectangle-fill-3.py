5279bbd1ce6bd4368655a07eb86dd588531c5f36
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-02-02 23:09:29 +0900

    Title: 사각형 채우기 3, Time: 47ms, Memory: 16MB, Status: Passed - Codetree

--- 260202/사각형 채우기 3/rectangle-fill-3.py
+++ 260202/사각형 채우기 3/rectangle-fill-3.py
@@ -1,14 +1,29 @@
-N = int(input())
+import sys
 
-dp = [ -1 ] * (N+1)
+def solve():
+    n = int(sys.stdin.readline())
+    MOD = 1000000007
+    
+    if n == 0:
+        print(1)
+        return
+    if n == 1:
+        print(2)
+        return
+    if n == 2:
+        print(7)
+        return
 
-dp[0] = 1
-dp[1] = 2
+    # dp 테이블 초기화
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 2
+    dp[2] = 7
 
-for i in range(2, N+1):
-    if i == 2 :
-        dp[2] = 7
-    else:
-        dp[i] = dp[i-3] * 2 + dp[i-2] * 3 + dp[i-1] * 2
+    # 점화식 적용: dp[i] = 3 * dp[i-1] + dp[i-2] - dp[i-3]
+    for i in range(3, n + 1):
+        dp[i] = (3 * dp[i-1] + dp[i-2] - dp[i-3]) % MOD
+        
+    print(dp[n])
 
-print(dp[N])
+solve()
