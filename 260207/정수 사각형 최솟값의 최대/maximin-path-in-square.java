c5e42e01d9403f714458c452e558f004ce4a3486
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-02-07 22:42:14 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 346ms, Memory: 24MB, Status: Passed - Codetree

--- 260207/정수 사각형 최솟값의 최대/maximin-path-in-square.java
+++ 260207/정수 사각형 최솟값의 최대/maximin-path-in-square.java
@@ -26,11 +26,12 @@ public class Main {
 
        for(int i = 1; i < N; i++){
         for(int j = 1; j < N; j++){
-            if ((i == N-1) && (j == N-1)){
-                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-            } else{
-                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]);
-            }
+
+            int v1 =  Math.min(dp[i-1][j], mat[i][j]);
+            int v2 = Math.min(dp[i][j-1], mat[i][j]);
+
+            dp[i][j] = Math.max(v1, v2);
+
         }
        }
 
