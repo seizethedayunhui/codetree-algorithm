3333ddf8d6945efd3a33ae116f6483fa83e37b9b
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-02-20 21:52:46 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 69ms, Memory: 17MB, Status: Passed - Codetree

--- 260220/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 260220/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -1,32 +1,41 @@
-N = int(input())
-points = [ list(map(int, input().split())) for _ in range(N) ]
+# 변수 선언 및 입력:
+n = int(input())
+segments = [
+    tuple(map(int, input().split()))
+    for _ in range(n)
+]
 
-dp = [ -1 for _ in range(1, 10001) ]
+# dp[i] : x1 기준으로 정렬되어 있다는 가정 하에서
+#         i번째 선분을 끝으로
+#         겹치지 않게 선택할 수 있는 최대 선분의 수
+dp = [0] * n
 
-cnt = N
+# x1 기준으로 오름차순 정렬을 진행합니다.
+segments.sort()
 
-for point in points:
-    x1 = point[0]
-    x2 = point[1]
+for i in range(n):
+    # 현재 선분이 시작 선분인 경우에는
+    # dp값이 1이 되므로
+    # 초기 셋팅은 1입니다.
+    dp[i] = 1
 
-    l = abs(x2-x1+1)
-    min_l = l
+    # i번째 선분 선택 전에
+    # 바로 선택한 선분을 j라 했을 때 
+    # i, j 선분이 서로 겹치지 않는 경우 중 
+    # 선택 할 수 있는 선분의 최대 개수를 계산합니다.
+    for j in range(i):
+        x1_i, _ = segments[i]
+        _, x2_j = segments[j]
+        
+        # 이미 x1 순으로 정렬이 되어있기에
+        # x2[j] < x1[i]이기만 하면 두 선분은 겹치지 않습니다.
+        if x2_j < x1_i:
+            dp[i] = max(dp[i], dp[j] + 1)
 
-    for i in range(x1, x2+1):
+# 마지막으로 선택한 선분 위치가 i일 때의 경우 중
+# 고를 수 있는 선분의 수가 가장 큰 경우를 고릅니다.
+ans = 0
+for i in range(n):
+    ans = max(ans, dp[i])
 
-        if dp[i] > 0:
-            min_l = min(dp[i], l)
-            dp[i] = min_l
-        else:
-            dp[i] = l
-
-    if min_l < l:
-        cnt -= 1
-        for j in range(x1, x1+1):
-            if dp[j] != min_l:
-                dp[i] = -1
-
-# for idx in range(1, 100):
-#     print(dp[idx], end=" ")
-
-print(cnt)
+print(ans)
