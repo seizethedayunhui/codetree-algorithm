35a5e3dc5c64ae11703aa05142405fef8beb93e4
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-02-23 00:26:36 +0900

    Title: 알바로 부자 되기, Time: 87ms, Memory: 18MB, Status: Wrong Answer - Codetree

--- 260223/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 260223/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -12,18 +12,23 @@ def check(s1, e1, s2, e2):
 for i in range(1, N):
 
     s1, e1, p1 = works[i]
+    dp[i] = p1
 
     for j in range(i):
 
         s2, e2, p2 = works[j]
 
         if check(s1, e1, s2, e2):
-            dp[i] = max(dp[j], p1)
+            dp[i] = max(dp[j], dp[i])
         else:
             dp[i] = max(dp[j] + p1, dp[i])
 
-# for i in range(N):
-#     print(dp[i], end=" ")
+    #     print(i, j)
+    #     for k in range(N):
+    #         print(dp[k], end=" ")
+    #     print()
+    # print()
+
 
 ans = dp[N-1]
 print(ans)
