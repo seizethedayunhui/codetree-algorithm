437cbdfd81dbc1032ce98a97a20291b7da9cfd47
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-02-26 20:33:52 +0900

    Title: 배낭 채우기, Time: 76ms, Memory: 18MB, Status: Passed - Codetree

--- 260226/배낭 채우기/knapsack.py
+++ 260226/배낭 채우기/knapsack.py
@@ -17,12 +17,19 @@ dp[0] = 0
 
 for i in range(N):
     for j in range(M, -1,-1):
-
-        if dp[j-weight[i]] == MIN_VALUE:
-            continue
             
         if j >= weight[i]:
+
+            #print(dp)
+
+            if dp[j-weight[i]] == MIN_VALUE:
+                continue
+
             dp[j] = max(dp[j], dp[j-weight[i]] + value[i])
 
-ans = dp[M]
+ans = float('-inf')
+
+for i in range(M+1):
+    ans = max(ans, dp[i])
+
 print(ans)
