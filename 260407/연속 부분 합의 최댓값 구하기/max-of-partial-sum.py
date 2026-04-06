7e771349bbbb0789550a1740dcbac53c3228e448
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-04-07 03:02:23 +0900

    Title: 연속 부분 합의 최댓값 구하기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260407/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
+++ 260407/연속 부분 합의 최댓값 구하기/max-of-partial-sum.py
@@ -3,9 +3,9 @@ arr = list(map(int, input().split()))
 
 dp = [ float('-inf') for _ in range(n+1) ]
 
-dp[0] = arr[0]
+dp[1] = arr[0]
 
-for i in range(1, n+1):
+for i in range(2, n+1):
     dp[i] = max(dp[i-1] + arr[i-1], arr[i-1])
 
 answer = float('-inf')
