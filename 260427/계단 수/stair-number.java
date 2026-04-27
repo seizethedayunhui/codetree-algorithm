3bd32574a0ebc0b0e4ee013a696068bc4b9dbc56
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-04-27 23:55:29 +0900

    Title: 계단 수, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260427/계단 수/stair-number.java
+++ 260427/계단 수/stair-number.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        if (!sc.hasNextInt()) return;
         int n = sc.nextInt();
-        // Please write your code here.
 
-        int[][] dp = new int[n+1][10];
-
-        for(int i = 1; i < 10; i++){
+    
+        long[][] dp = new long[n + 1][10];
+        long mod = 1000000007L; 
+    
+        for (int i = 1; i < 10; i++) {
             dp[1][i] = 1;
         }
 
-        for(int i = 2; i < n+1; i++){
-            for(int j = 0; j < 10; j++){
-                for(int k = 0; k < 10; k++){
-
-                    if(Math.abs(j-k) == 1){
-                        dp[i][j] += dp[i-1][k];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) { 
+                for (int k = 0; k < 10; k++) { 
+                    
+                    if (Math.abs(j - k) == 1) {
+                    
+                        dp[i][j] = (dp[i][j] + dp[i-1][k]) % mod;
                     }
                 }
             }
         }
 
-        int ans = 0;
-        for(int i = 0; i < 10; i++){
-            ans += dp[n][i];
+        long ans = 0;
+        for (int i = 0; i < 10; i++) {
+            ans = (ans + dp[n][i]) % mod;
         }
-        ans = ans % ((int) Math.pow(10, 9) + 7);
+        
         System.out.println(ans);
     }
 }
