4022dd94c43dcb9e45b41da18818ab13e3412514
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-04-27 23:04:52 +0900

    Title: 적절한 옷 고르기, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260427/적절한 옷 고르기/select-proper-clothes.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] s = new int[n+1];
+        int[] e = new int[n+1];
+        int[] v = new int[n+1];
+        for (int i = 1; i < n+1; i++) {
+            s[i] = sc.nextInt();
+            e[i] = sc.nextInt();
+            v[i] = sc.nextInt();
+        }
+
+        int[][] dp = new int[m+1][n+1];
+
+        for(int i = 0; i < m+1; i++){
+            for(int j = 0; j < n+1; j++){
+                dp[i][j] = Integer.MIN_VALUE;
+            }
+        }
+
+        for(int i = 1; i < n+1; i++){
+            if ( s[i] <= 1 && e[i] >= 1 ){
+                dp[1][i] = 0;
+            }
+        }
+
+        for(int i = 2; i < m+1; i++){
+            for(int j = 1; j < n+1; j++){
+
+                // check possible clothes
+                if (s[j] > i || e[j] < i) continue;
+
+                for(int k = 1; k < n+1; k++){
+                    
+                    if (dp[i-1][k] == Integer.MIN_VALUE) continue;
+
+                    int statisfac = Math.abs(v[j]-v[k]);
+                    dp[i][j] = Math.max(dp[i-1][k] + statisfac, dp[i][j]);
+
+                }
+            }
+        }
+
+        // for(int i = 0; i < m+1; i++){
+        //     for(int j = 0; j < n+1; j++){
+        //         System.out.print(dp[i][j] + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int ans = Integer.MIN_VALUE;
+
+        for(int i = 1; i < n+1; i++){
+            ans = Math.max(dp[m][i], ans);
+        }
+
+        System.out.println(ans);
+
+    }
+}
