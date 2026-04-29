3410695d14c2df490e95d4c5239003cf419b9793
yuni <113826033+seizethedayunhui@users.noreply.github.com>
2026-04-29 22:20:16 +0900

    Title: 신전 탐험하기, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260429/신전 탐험하기/explore-temple.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] data = new int[N+1][4];
+        for (int i = 1; i < N+1; i++) {
+            data[i][1] = sc.nextInt();
+            data[i][2] = sc.nextInt();
+            data[i][3] = sc.nextInt();
+        }
+        // Please write your code here.
+
+        int[][] dp = new int[N+1][4];
+        for(int i = 1; i < 4; i++){
+            dp[1][i] = data[1][i];
+        }
+
+        for(int i = 2; i < N+1; i++){
+            for(int j = 1; j < 4; j++){
+                for(int k = 1; k < 4; k++){
+                    if (j==k) continue;
+
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][k] + dp[i][j]);
+                }
+            }
+        }
+
+        int ans = Integer.MIN_VALUE;
+        for(int i = 1; i < 4; i++){
+            ans = Math.max(ans, dp[N][i]);
+        }
+
+        System.out.println(ans);
+    }
+}
